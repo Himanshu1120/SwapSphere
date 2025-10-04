b04a3af710691d76804348f9d0fb74e6f87eb10a
Himanshu <hkchaudhari21@gmail.com>
2025-10-04 13:57:20 +0530

    Initial commit

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { logout, login, signup, refreshToken, getProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+router.post("/login",login);
+router.post("/logout", logout)
+router.post("/refresh-token", refreshToken)
+router.get("/profile", protectRoute, getProfile);
+
+
+export default router;
+
+
+// Z8kNdyJyyw3Oa60X
+// Z8kNdyJyyw3Oa60X
